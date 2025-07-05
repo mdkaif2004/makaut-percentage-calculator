d075162125d75f3a6aa8f6aeac3ffacb43d9ae6f
mdkaif2004 <mallickmdkaif@gmail.com>
2025-07-05 09:55:10 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from app import app
 from admin import admin_bp
+import os
 
 app.register_blueprint(admin_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # use Render's assigned port, or 10000 locally
+    app.run(debug=False, host='0.0.0.0', port=port)
+    
